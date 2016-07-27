33f2c3ba97070748b314b625cd1204d634a9fcf4
vporta <vporta7@gmail.com>
2016-07-27 00:20:57 -0400

    push

--- public/js/scrolling-nav.js
+++ public/js/scrolling-nav.js
@@ -17,3 +17,11 @@ $(function() {
         event.preventDefault();
     });
 });
+
+$(window).scroll(function(){
+    if($(document).scrollTop()>=$(document).height()/5)
+        $("#spopup").show("slow");else $("#spopup").hide("slow");
+});
+function closeSPopup(){
+    $('#spopup').hide('slow');
+}
