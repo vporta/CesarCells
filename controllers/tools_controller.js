73a4fd829216f0d064150241b2318e44be6d7b1e
vporta <vporta7@gmail.com>
2016-08-11 03:03:07 -0400

    data displays on pagegit add .

--- controllers/tools_controller.js
+++ controllers/tools_controller.js
@@ -6,6 +6,7 @@ var bcrypt = require('bcryptjs');
 var path = require('path');
 var passport = require('passport');
 var Trial = require('../models/Trial.js');
+var SNPs = require('../models/SNPs.js');
 var oauth2 = require('simple-oauth2');
 var Amsler = require('../models/Amsler.js');
 var User = require('../models/UserModel.js');
@@ -53,17 +54,19 @@ var oauth2 = require('simple-oauth2')({
   authorizationPath: '/authorize'
 });
 // *Declaration of all necessary variables needed to perform 23AndMe API Call
-var STARGARDT_SNPs = ['rs28938473', 'rs61753033', 'rs61753034'].join(' ');
-var LCA_SNPs = ['rs281865192'].join(' ');
-var MACD_SNPs = ['rs11200638', 'rs1061170', 'rs800292', 'rs2230199', 'rs3775291'].join(' ');
-var RETPIG_SNPs = ['rs104893775'].join(' ');
-var USHER_SNPs = ['rs104894651'].join(' ');
-var GLAUCOMA_SNPs = ['rs28936694', 'rs1048661', 'rs3825942'].join(' ');
-var DEFAULT_SCOPE = 'user ancestry analyses relatives genomes ' + STARGARDT_SNPs;
+// var STARGARDT_SNPs = ['doesnt work:rs28938473', 'rs61753033', 'rs61753034'].join(' ');
+// var LCA_SNPs = ['doesnt work: rs281865192'].join(' ');
+// var MACD_SNPs = ['rs11200638', 'rs1061170', 'rs800292', 'rs2230199', 'rs3775291'].join(' ');
+// var RETPIG_SNPs = ['doesnt work: rs104893775'].join(' ');
+// var USHER_SNPs = ['doesnt work: rs104894651'].join(' ');
+// var GLAUCOMA_SNPs = ['rs28936694', 'rs1048661', 'rs3825942'].join(' ');
+// var DEFAULT_SCOPE = 'basic names analyses rs28938473';
+
+// console.log(DEFAULT_SCOPE);
 
 var authorization_uri = oauth2.authCode.authorizeURL({
   redirect_uri: 'http://localhost:3000/receive_code/',
-  scope: DEFAULT_SCOPE,
+  scope: 'basic names analyses rs61753033 rs61753034 rs11200638 rs1061170 rs800292 rs2230199 rs3775291 rs28936694 rs1048661 rs3825942',
   state: 'angie1'
 });
 
@@ -118,12 +121,13 @@ router.get('/receive_code', function(req, res) {
 
 router.get('/tools/genetic-data-retinal-diseases', function(req, res) {
 
-  var dataMediate = {}; // ==== MEDIATOR ====
+  // var dataMediate = {}; // ==== MEDIATOR ====
 
   if (req.signedCookies.access_token) {
+    
     console.log('its signed' + req.signedCookies.access_token);
     
-    var genotypes, user;
+    var names, names_by_id = {}, genotypes;
 
     var base_uri = 'https://api.23andme.com/1';
     
@@ -131,7 +135,7 @@ router.get('/tools/genetic-data-retinal-diseases', function(req, res) {
 
     axios({ 
       
-      url: base_uri + '/user/', 
+      url: base_uri + '/names/', 
       
       headers: headers
     
@@ -145,10 +149,10 @@ router.get('/tools/genetic-data-retinal-diseases', function(req, res) {
       
       } else {
 
-          user = response.data;
-          dataMediate.userId = user.profiles[0].id;
+          // user = response.data;
+          // dataMediate.userId = user.profiles[0].id;
 
-          console.log('*********user profile id:  ' + user.profiles[0].id);
+          console.log('*********user profile id:  ' + response.data.profiles[0].id);
 
           // for (var i = 0; i < names.profiles.length; i++) {
           //   names_by_id[names.profiles[i].id] = names.profiles[i].first_name + ' ' + names.profiles[i].last_name;
@@ -156,26 +160,37 @@ router.get('/tools/genetic-data-retinal-diseases', function(req, res) {
 
           axios({
             
-            url: base_uri + '/genotypes/'+ user.profiles[0].id +'/?locations=' + , 
+            url: base_uri + '/genotypes/' + response.data.profiles[0].id + '/?locations=rs61753033 rs61753034 rs11200638 rs1061170 rs800292 rs2230199 rs3775291 rs28936694 rs1048661 rs3825942', 
             
             headers: headers
 
           })
           .then(function(response) {
+            
 
+            
             genotypes = response.data;
 
-            console.log('======genotypes:  ' + genotypes);
+            console.log('======genotypes:  ' + JSON.stringify(genotypes));
 
-            console.log('======SNP: ' + genotypes.rs3094315)
+            
+              var obj = genotypes;
 
-            res.render('tools/gene_data', {
-              
-              layout: 'dash',
-              genotypes: genotypes
+              // for(key in obj) {
+              //   if (obj.hasOwnProperty(key)) {
+              //       console.log(key + " -> " + obj[key]);
+                      
+              //     }
+              // }
+              res.render('tools/gene_data', {
+                layout: 'dash',
+                obj: obj
 
-            });
+              });
+            
 
+
+            
           })
           .catch(function(error) {
             console.log(error);
@@ -187,8 +202,11 @@ router.get('/tools/genetic-data-retinal-diseases', function(req, res) {
       console.log(error);
     });
 
+    // console.log('=====LOOK OVER HERE_____===genotypes:  ' + JSON.stringify(genotypes));
+
     // res.render('tools/gene_data', {
-    //   layout: 'dash'
+    //   layout: 'dash',
+    //   genotypes: genotypes
 
     // });
   } else {
---
+++ models/SNPs.js
@@ -0,0 +1,16 @@
+// Include the Mongoose Dependencies
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// Create a Schema for capturing clicks. We'll use clickID to update the same clickCounter
+var SNPsSchema = new Schema({
+  snp: String,
+  user_id: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+
+});
+
+// Create the Model
+var SNPs = mongoose.model('SNPs', SNPsSchema);
+
+// Export it for use elsewhere
+module.exports = SNPs;
