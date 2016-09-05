8c522a30c09a3b54d6a1c8e26b8cabd1c7cf58b5
vporta <vporta7@gmail.com>
2016-09-05 02:24:07 -0400

    styled index page

--- controllers/resources_controller.js
+++ controllers/resources_controller.js
@@ -22,9 +22,9 @@ router.get('/stargardts-glossary', function(req, res) {
   res.render('resources/glossary');
 });
 
-router.get('/stargardts-stemcell-story', function(req, res) {
-  res.render('resources/story_cesar');
-});
+// router.get('/stargardts-stemcell-story', function(req, res) {
+//   res.render('resources/story_cesar');
+// });
 
 router.get('/stargardts-assistive-technologies', function(req, res) {
   res.render('resources/assistive_tech_vendors');
