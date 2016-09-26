5ba7c5e7c595d4852929f888a2a6f80868448be9
vporta <vporta7@gmail.com>
2016-09-26 18:35:42 -0400

    again..

--- controllers/tools_controller.js
+++ controllers/tools_controller.js
@@ -122,7 +122,7 @@ var oauth2 = require('simple-oauth2')({
 // });
 
 var authorization_uri = oauth2.authCode.authorizeURL({
-  redirect_uri: 'https://arcane-fortress-13823.herokuapp.com/receive_code/',
+  redirect_uri: 'https://www.cesarcells.com/receive_code/',
   scope: 'basic names analyses rs61753033 rs61753034 rs11200638 rs1061170 rs800292 rs2230199 rs3775291 rs28936694 rs1048661 rs3825942',
   state: 'angie1'
 });
@@ -161,7 +161,7 @@ router.get('/receive_code', ensureAuthenticated, function(req, res) {
         
       code: code,
         
-      redirect_uri: 'https://arcane-fortress-13823.herokuapp.com/receive_code/'
+      redirect_uri: 'https://www.cesarcells.com/receive_code/'
       
     }, saveToken);
      
